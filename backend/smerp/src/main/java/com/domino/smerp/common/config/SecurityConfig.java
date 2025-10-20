ee76a5526cb2265412e172209535a9ebc51a782b
wjdvlf5456 <wjdvlf1407@gmail.com>
2025-10-20 17:12:44 +0900

    [feat] 시큐리티 통합테스트 구현

--- backend/smerp/src/main/java/com/domino/smerp/common/config/SecurityConfig.java
+++ backend/smerp/src/main/java/com/domino/smerp/common/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -27,10 +28,17 @@ public class SecurityConfig {
             .cors(customizer ->
                 customizer.configurationSource(getCorsConfigurationSource()))
             .authorizeHttpRequests(
-                auth -> auth.requestMatchers("/api/v1/**")
-                            .permitAll()
-                            .anyRequest()
-                            .authenticated()
+                auth -> auth.requestMatchers("/api/v1/auth/**") // 로그인/로그아웃/me는 모두 허용 (인증 전에도 접근 가능)
+                    .permitAll()
+                    // User Controller에 대한 인가 규칙 추가
+                    .requestMatchers(HttpMethod.PATCH, "/api/v1/users/**") // 사용자 수정
+                    .hasAnyRole("ADMIN", "MANAGER") // ADMIN 또는 MANAGER만 허용
+                    .requestMatchers(HttpMethod.POST, "/api/v1/users") // 사용자 생성
+                    .hasAnyRole("ADMIN", "MANAGER") // ADMIN 또는 MANAGER만 허용
+                    .requestMatchers(HttpMethod.DELETE, "/api/v1/users/**") // 사용자 삭제
+                    .hasRole("ADMIN") // ADMIN만 허용
+                    .anyRequest()
+                    .authenticated() // 그 외의 모든 요청은 인증된 사용자에게만 허용
             )
             .anonymous(anonymous -> anonymous
                 .principal("SYSTEM"))
